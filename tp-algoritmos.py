99a7cb436cd944f20cdca7c13aa6c08a1b0ea6f0
Francisco Fuhr <francisco.fuhr@valtech.com>
2024-08-26 20:54:18 -0300

    Initial Commit

--- tp-algoritmos.py
+++ tp-algoritmos.py
@@ -1,6 +1,15 @@
 import csv
 
-with open('datosAlumnos.csv', newline='') as archivo_csv:
-    lector_csv = csv.reader(archivo_csv, delimiter=',', quotechar='"')
-    for fila in lector_csv:
-        print(fila)
+with open('datosAlumnos.csv', mode='r') as archivo_csv:
+    lector_csv = csv.DictReader(archivo_csv, delimiter=';')  # Delimitador es una coma
+    lista_diccionarios = [fila for fila in lector_csv]
+for persona in lista_diccionarios:
+    print(persona['Nombre'])
+
+## funcion que lee las llaves de la lista
+
+def leer_llaves_diccionario(diccionario):
+    for persona in diccionario:
+     print(persona.keys())
+
+leer_llaves_diccionario(lista_diccionarios)
