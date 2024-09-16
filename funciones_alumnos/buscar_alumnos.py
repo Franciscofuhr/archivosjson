58b058d56b82892e1d038fb73f2df0cd4eb3ec11
Mati <mbarite@uade.edu.ar>
2024-09-16 18:49:20 -0300

    Correcciones en UI

--- funciones_alumnos/buscar_alumnos.py
+++ funciones_alumnos/buscar_alumnos.py
@@ -1,18 +1,19 @@
-from colorama import Fore, Style
+from colorama import Fore, Back, Style
 
 
 def buscar_alumno(lista_diccionarios):
     bandera_principal = True
 
     while bandera_principal:
-        print(f"{Fore.YELLOW}--- Opciones de búsqueda ---{Style.RESET_ALL}")
+        print(f"\n{Back.WHITE + Fore.BLUE}--- Opciones de búsqueda ---{Back.RESET}\n")
         print(f"{Fore.CYAN}1. {Fore.RESET}Buscar por ID")
         print(f"{Fore.CYAN}2. {Fore.RESET}Buscar por Nombre")
         print(f"{Fore.CYAN}3. {Fore.RESET}Buscar por Apellido")
-        opcion = input(f"{Fore.GREEN}Elige una opción (1, 2, 3 o 4 para volver al menú principal): ")
+        print(f"{Fore.CYAN}4. {Fore.RESET}Volver al menu principal")
+        opcion = input(f"\n{Back.WHITE + Fore.BLUE}Elige una opción (1, 2, 3 o 4 para volver al menú principal): {Back.RESET}")
 
         if opcion == '4':
-            print(f"{Fore.YELLOW}Volviendo al menú principal...{Style.RESET_ALL}")
+            print(f"\n{Fore.RED + Style.BRIGHT}Volviendo al menú principal...")
             bandera_principal = False
             continue
 
@@ -21,9 +22,9 @@ def buscar_alumno(lista_diccionarios):
 
         while bandera_busqueda:
             if opcion == '1':
-                id_alumno = input(f"{Fore.CYAN}Introduce el ID del alumno o -1 para regresar al menú de búsqueda: ")
+                id_alumno = input(f"\n{Back.WHITE + Fore.BLUE}Introduce el ID del alumno o -1 para regresar al menú de búsqueda: {Back.RESET}")
                 if id_alumno == '-1':
-                    print(f"{Fore.YELLOW}Volviendo al menú de búsqueda...")
+                    print(f"\n{Fore.RED + Style.BRIGHT}Volviendo al menú de búsqueda...")
                     bandera_busqueda = False
                     continue
 
@@ -32,33 +33,33 @@ def buscar_alumno(lista_diccionarios):
                         resultado = persona
 
             elif opcion == '2':
-                nombre = input(f"{Fore.CYAN}Introduce el nombre del alumno o -1 para regresar al menú de búsqueda: ")
+                nombre = input(f"\n{Back.WHITE + Fore.BLUE}Introduce el nombre del alumno o -1 para regresar al menú de búsqueda: {Back.RESET}")
                 if nombre == '-1':
-                    print(f"{Fore.YELLOW}Volviendo al menú de búsqueda...")
+                    print(f"{Fore.RED + Style.BRIGHT}Volviendo al menú de búsqueda...")
                     bandera_busqueda = False
                     continue
                 for persona in lista_diccionarios:
-                    if persona['Nombre'] == nombre:
+                    if persona['Nombre'].lower() == nombre:
                         resultado = persona
 
             elif opcion == '3':
-                apellido = input(f"{Fore.CYAN}Introduce el apellido del alumno o -1 para regresar al menú de búsqueda: ")
+                apellido = input(f"\n{Back.WHITE + Fore.BLUE}Introduce el apellido del alumno o -1 para regresar al menú de búsqueda: {Back.RESET}")
                 if apellido == '-1':
-                    print(f"{Fore.YELLOW}Volviendo al menú de búsqueda...")
+                    print(f"\n{Fore.RED + Style.BRIGHT}Volviendo al menú de búsqueda...")
                     bandera_busqueda = False
                     continue
                 for persona in lista_diccionarios:
-                    if persona['Apellido'] == apellido:
+                    if persona['Apellido'].lower() == apellido:
                         resultado = persona
 
             else:
-                print(f"{Fore.YELLOW}Opción no válida.")
+                print(f"{Fore.RED}Opción no válida.")
                 bandera_busqueda = False
                 continue
 
             # Imprimir resultado de la búsqueda
             if resultado:
-                print(f"{Fore.MAGENTA}Legajo: {resultado['Legajo']}, {resultado['Nombre']} {resultado['Apellido']}: 1er Parcial: {resultado['nota1']} / 2do Parcial: {resultado['nota2']}")
+                print(f"\n{Fore.YELLOW}Legajo: {resultado['Legajo']}, {resultado['Nombre']} {resultado['Apellido']}: 1er Parcial: {resultado['nota1']} / 2do Parcial: {resultado['nota2']}")
             else:
                 print(f"{Fore.RED}No se encontraron coincidencias")
             resultado = None
--- funciones_parcial/buscar_resultados_parcial.py
+++ funciones_parcial/buscar_resultados_parcial.py
@@ -1,12 +1,15 @@
 from variables_globales import NOTA_APROBACION
-from colorama import Fore, Style
+from colorama import Fore, Back, Style
 
 
 def mostrar_menu_aprobados_desaprobados():
-    print(f"{Fore.YELLOW}\n--- Opciones ---{Style.RESET_ALL}")
+    print()
+    print(f"{Back.WHITE + Fore.BLUE + Style.BRIGHT}--- Opciones ---")
+    print()
     print(f"{Fore.CYAN}1. {Fore.RESET}Aprobados")
     print(f"{Fore.CYAN}2. {Fore.RESET}Desaprobados")
-    print(f"{Fore.CYAN}3. {Fore.RESET}Salir")
+    print(f"{Fore.CYAN}3. {Fore.RESET}Volver al menu principal")
+    print()
 
 
 def clasificar_parcial(alumnos, parcial, nota_aprobacion):
@@ -42,33 +45,42 @@ def ejecutar_opcion_parcial(opcion_parcial, lista_diccionarios):
         elif opcion_parcial == '2':
             parcial = 'nota2'
             parcial_nombre = '2do Parcial'
+        elif opcion_parcial == '3':
+            print()
+            print(f"{Fore.RED + Style.BRIGHT}--- Volviendo al menú principal... ---")
+            bandera = False
+            continue
         else:
             print(f"{Fore.RED}Opción no válida. Intenta nuevamente.")
             return
 
         mostrar_menu_aprobados_desaprobados()
 
-        opcion_aprobado_desaprobado = input(f"{Fore.GREEN}Selecciona una opción (1, 2 o 3): ").lower()
+        opcion_aprobado_desaprobado = input(f"{Back.WHITE + Fore.BLUE + Style.BRIGHT}Selecciona una opción (1, 2 o 3): {Back.RESET + Fore.RESET + Style.RESET_ALL}").lower()
 
         aprobados, desaprobados = clasificar_parcial(lista_diccionarios, parcial, NOTA_APROBACION)
 
         if opcion_aprobado_desaprobado == '1':
-            print(f"{Fore.YELLOW}\n--- Aprobados {parcial_nombre} ---")
+            print()
+            print(f"{Fore.GREEN + Style.BRIGHT}--- Aprobados {parcial_nombre} ---")
+            print()
             for alumno in aprobados:
-                print(Fore.MAGENTA + f"Legajo: {alumno[0]}, {alumno[1]} {alumno[2]}: {alumno[3]}" + Style.RESET_ALL)
+                print(Fore.GREEN + f"Legajo: {alumno[0]}, {alumno[1]} {alumno[2]}: {alumno[3]}" + Style.RESET_ALL)
             if len(aprobados) == 0:
                 print(f"\n{Fore.RED}No hay alumnos aprobados.")
             continue
         elif opcion_aprobado_desaprobado == '2':
-            print(f"{Fore.YELLOW}\n--- Desaprobados {parcial_nombre} ---")
+            print(f"{Fore.RED + Style.BRIGHT}\n--- Desaprobados {parcial_nombre} ---")
+            print()
             for alumno in desaprobados:
-                print(Fore.MAGENTA + f"Legajo: {alumno[0]}, {alumno[1]} {alumno[2]}: {alumno[3]}" + Style.RESET_ALL)
+                print(Fore.RED + f"Legajo: {alumno[0]}, {alumno[1]} {alumno[2]}: {alumno[3]}" + Style.RESET_ALL)
             if len(desaprobados) == 0:
                 print(f"\n{Fore.RED}No hay alumnos desaprobados.")
             continue
         elif opcion_aprobado_desaprobado == '3':
-            print(f"{Fore.RED}\n--- Saliendo... ---")
+            print()
+            print(f"{Fore.RED + Style.BRIGHT}--- Volviendo al menu principal... ---")
             bandera = False
 
         else:
-            print(f"{Fore.RED}Opción no válida. Intenta nuevamente.")
+            print(f"{Fore.RED + Back.WHITE}Opción no válida. Intenta nuevamente.")
